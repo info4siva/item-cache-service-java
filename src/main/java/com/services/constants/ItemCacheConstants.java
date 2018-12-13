7b571231e4a5860655aabb43f5fe3c21a995f531
Siva Ramakrishnan <12631394+info4siva@users.noreply.github.com>
2018-12-13 10:13:37 -0800

    Update ItemCacheConstants.java
    Updated min-record count

--- src/main/java/com/services/constants/ItemCacheConstants.java
+++ src/main/java/com/services/constants/ItemCacheConstants.java
@@ -7,7 +7,7 @@ package com.services.constants;
 public final class ItemCacheConstants {
     public static final int SERVER_CONFIG_PORT = 8080;
     public static final String SERVER_CONFIG_PATH_SPEC = "/api/*";
-    public static final int MIN_RECORDS_IN_CACHE = 3;
+    public static final int MIN_RECORDS_IN_CACHE = 100;
     public static final long TIMEOUT_IN_MILLIS = 2000L;
     public static final int GUAVA_CONCURRENCY_LEVEL = 16;
     public static final int MAX_RECORDS_IN_CACHE = 10000;
